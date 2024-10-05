d125311e0f7f1df267333ac66d7098d09888a489
vuphuong1794 <phuongvu1794@gmail.com>
2024-10-05 15:18:35 +0700

    add fe files

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello nextjs and nestjs</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
